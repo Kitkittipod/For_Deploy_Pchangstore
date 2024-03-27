da502845d1dd727f0249bb22342e364be2b69f0c
Kitkittipod <165009720+Kitkittipod@users.noreply.github.com>
2024-03-27 17:39:30 +0700

    Update sChart.js

--- src/app/components/sChart.js
+++ src/app/components/sChart.js
@@ -42,7 +42,7 @@ export function SaleChart() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://your-backend.vercel.app/getMonthlySales');
+        const response = await fetch('https://store-backend-ruddy.vercel.app/getMonthlySales');
         if (!response.ok) {
           throw new Error('Failed to fetch data');
         }
