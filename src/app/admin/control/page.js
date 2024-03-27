6bf5e695d7c666ee6d8eac673868d2f9854ed36f
Kitkittipod <165009720+Kitkittipod@users.noreply.github.com>
2024-03-27 20:49:54 +0700

    Update page.js

--- src/app/admin/control/page.js
+++ src/app/admin/control/page.js
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 
-export default function control() {
+export default function Control() {
 
   // const [socket, setSocket] = useState(null);
   const [orders, setOrders] = useState([])
