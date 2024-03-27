876752fec993ea3bb6f95f85af497d3b5903576d
Kitkittipod <165009720+Kitkittipod@users.noreply.github.com>
2024-03-27 17:38:21 +0700

    Update page.js

--- src/app/admin/control/page.js
+++ src/app/admin/control/page.js
@@ -8,10 +8,10 @@ export default function Control() {
   const [refresh, setRefresh] = useState(false)
 
   useEffect(() => {
-    fetch('https://your-backend.vercel.app/getOrder')
+    fetch('https://store-backend-ruddy.vercel.app/getOrder')
     .then((res) => res.json())
     .then((data) => setOrders(data))
-    fetch('https://your-backend.vercel.app/getMenuAvailability')
+    fetch('https://store-backend-ruddy.vercel.app/getMenuAvailability')
     .then((res) => res.json())
     .then((data) => setMenuData(data))
   }, [refresh])
@@ -75,7 +75,7 @@ export default function Control() {
     }
 
     try{
-      const response = await fetch('https://your-backend.vercel.app/api/changeStatus', {
+      const response = await fetch('https://store-backend-ruddy.vercel.app/changeStatus', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(body),
